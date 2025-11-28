a8f0a1825e7da73c3fdb7c1b35afb5ccf9023758
Biologist <soumithradebnathshanto@gmail.com>
2025-11-28 12:43:29 +0600

    Done

---
+++ New code/Template/Main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pf push_front
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(x) (int)(x).size()
+#define ff first
+#define ss second
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+const ld PI = 3.141592653589793238462;
+const ld EPS = 1e-9;
+const int N = 3e5 + 9;
+
+template <typename T>
+void read(vector<T> &v) {
+    for (auto &x : v) cin >> x;
+}
+
+template <typename T>
+void show(const T &container) {
+    for (const auto &x : container) cout << x << " ";
+    cout << "\n";
+}
+
+void solve() {
+    cout << "hi";
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
