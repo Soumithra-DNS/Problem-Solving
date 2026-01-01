c02deb4854c6158527a7129ac665e501a20febd0
Biologist <soumithradebnathshanto@gmail.com>
2026-01-01 20:04:23 +0600

    done

--- New code/Template/Cycle Detection.cpp
+++ New code/Template/Cycle Detection.cpp
@@ -1,43 +1,10 @@
-// Cycle detection algorithm (Directed Graph)
-const int N = 1e5 + 5;
-vector<int> adj[N];
-int vis[N], col[N], pr[N];
-
-void dfs(int node, int par)
-{
-    if (vis[node] == 2)
-    {
-        return;
-    }
-    if (vis[node] == 1)
-    {
-        col[node] = 1;
-        int x = par;
-        while (x != node)
-        {
-            col[x] = 1;
-            x = pr[x];
-        }
-        return;
-    }
-    pr[node] = par;
-    vis[node] = 1;
-    for (auto child : adj[node])
-    {
-        if (pr[node] == child)
-        {
-            continue;
-        }
-        dfs(child, node);
-    }
-    vis[node] = 2;
-}
-
-// Cycle detection algorithm (undirected Graph)
+// Directed Graph Cycle Detection (DFS Template)
 
 const int N = 1e5 + 5;
 vector<int> adj[N];
 int vis[N], col[N], pr[N];
+
+bool hasCycle = false;
 
 void dfs(int node)
 {
@@ -49,17 +16,59 @@ void dfs(int node)
         {
             pr[child] = node;
             dfs(child);
+            if (hasCycle)
+                return;
         }
         else if (vis[child] == 1)
         {
-            // cycle found
+            hasCycle = true;
+
+            // mark cycle nodes
             col[child] = 1;
-            int x = node;
-            while (x != child)
-            {
+            for (int x = node; x != child; x = pr[x])
                 col[x] = 1;
-                x = pr[x];
-            }
+
+            return;
+        }
+    }
+
+    vis[node] = 2;
+}
+
+// Undirected Graph Cycle Detection (DFS Template)
+
+const int N = 1e5 + 5;
+vector<int> adj[N];
+int vis[N], col[N], pr[N];
+
+bool hasCycle = false;
+
+void dfs(int node, int parent)
+{
+    vis[node] = 1;
+    pr[node] = parent;
+
+    for (int child : adj[node])
+    {
+        if (child == parent)
+            continue;
+
+        if (vis[child] == 0)
+        {
+            dfs(child, node);
+            if (hasCycle)
+                return;
+        }
+        else if (vis[child] == 1)
+        {
+            hasCycle = true;
+
+            // mark cycle nodes
+            col[child] = 1;
+            for (int x = node; x != child; x = pr[x])
+                col[x] = 1;
+
+            return;
         }
     }
 
